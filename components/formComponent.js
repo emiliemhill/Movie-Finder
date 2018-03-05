97e33f553f8f481889cf5a74877cc7b541df87a1
Mohammed Fahad <mfahad24@users.noreply.github.com>
2018-03-05 12:45:05 -0500

    more changes to rating stars

--- components/formComponent.js
+++ components/formComponent.js
@@ -6,8 +6,17 @@
       $ctrl.keysearch = function (form) {
         console.log(form);
       }
+      $ctrl.rating = function () {
+        console.log($ctrl.firstRate); 
+   $ctrl.firstRate = 0;
+   $ctrl.readOnly = true;
+   $ctrl.onItemRating = function(rating){
+     alert('On Rating: ' + rating)
+   }
+};
+      }
     }
-  }
+
   angular
     .module("myApp")
     .component("formComponent", formComponent)
