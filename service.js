1a54eb07f2f1e55eb8910efdb849143d5546e9a0
Emilie Hill <emiliemhill@gmail.com>
2018-03-06 10:29:56 -0500

    added service functionality

--- service.js
+++ service.js
@@ -1,15 +1,32 @@
-(function() {
+(function () {
   function MovieService($http) {
 
     return {
-      getLongMovies : getLongMovies
+      getLongMovies: getLongMovies
     }
 
     function getLongMovies() {
-
+      var baseUrl = "https://api.themoviedb.org/3/discover/movie?api_key=a420712cee91c2aec196fe700c0ceb35&sort_by=popularity.desc&include_adult=false&include_video=false"
+      var genreIds = ["18", "53"]
+      var minLength = 180
+      var minLengthUrl = "&with_runtime.gte=" + minLength
+      var maxLength = 90
+      var maxLengthUrl = "&with_runtime.lte=" + maxLength
+      var minRating = 5
+      var minRatingUrl = "&vote_average.gte=" + minRatingUrl
+      var maxRating = 10
+      var maxRatingUrl = "&vote_average.lte=" + maxRatingUrl
+      genreIds.forEach(function (Id) {
+        var genreUrl = "&with_genres=" + Id
+        baseUrl += genreUrl
+      })
+      baseUrl += minLengthUrl
+      baseUrl += maxLengthUrl
+      baseUrl += minRatingUrl
+      baseUrl += maxRatingUrl
       return $http({
         method: "GET",
-        url: "https://api.themoviedb.org/3/discover/movie?api_key=a420712cee91c2aec196fe700c0ceb35&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_runtime.gte=180"
+        url: baseUrl
       }).then(function (response) {
         console.log("service", response.data.results);
         return response.data.results;
