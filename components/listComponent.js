f4eace8c1ead972528296446bfb53e1b4299559b
Tom Bianchi <atbianchi@gmail.com>
2018-03-06 15:54:14 -0500

    created list component -atb

---
+++ components/listComponent.js
@@ -0,0 +1,13 @@
+(function() {
+  var listComponent = {
+    templateUrl: `partials/list.html`,
+    controller: function() {
+
+    }
+  }
+
+  angular
+    .module("myApp")
+    .component("listComponent", listComponent);
+
+})();
