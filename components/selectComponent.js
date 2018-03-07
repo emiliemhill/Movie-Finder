53a8b48ff02e16e29f5fc93725ed794bde68a8ce
Mohammed Fahad <mfahad24@users.noreply.github.com>
2018-03-07 14:58:37 -0500

    changes

--- components/selectComponent.js
+++ components/selectComponent.js
@@ -3,23 +3,21 @@
     template: `<p id="para"> {{ $ctrl.movies[$ctrl.movieShow].title }}
       <img class="border-radius" ng-src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/{{ $ctrl.movies[$ctrl.movieShow].poster_path }}">
       <button ng-click="$ctrl.nextMovie()">Next Movie</button>
+      <button ng-click="$ctrl.showMovie()">Show Movie</button>
+<button
       <button ng-click="$ctrl.saveToList($ctrl.movies[$ctrl.movieShow])">Add To List</button>
     </p>`,
     controller: function(MovieService, ListService) {
       var vm = this;
       vm.movieShow = 0;
-      angular.element("p").ready(function(){
-        console.log("p");
-      })
       vm.nextMovie = function() {
         vm.movieShow++;
         console.log(vm.movieShow);
       }
-      // vm.showMovie = function() {
-      //       vm.movies = MovieService.setMovies().then(function(){
-      //
-      //       })
-      // }
+      vm.showMovie = function() {
+        vm.movies = MovieService.setMovies();
+      }
+
       vm.saveToList = function(movie) {
         // var movie = "movie";
         // console.log(movie);
