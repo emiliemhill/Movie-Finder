53e955e8641699f37a5d4fcf257ffd6d4379674b
Tom Bianchi <atbianchi@gmail.com>
2018-03-08 10:56:30 -0500

    infinite scroll -atb

--- components/selectComponent.js
+++ components/selectComponent.js
@@ -6,11 +6,27 @@
       vm.returned = MovieService.returnParams();
       MovieService.getMovies(vm.returned);
       vm.movies = MovieService.setMovies();
+      vm.page = 1;
 
       console.log(vm.returned);
       vm.nextMovie = function() {
         vm.movies.splice(0, 1);
         console.log(vm.movies);
+        if (vm.movies.length < 3) {
+
+          console.log("running out of movies");
+          vm.page++;
+          console.log(vm.page);
+          vm.returned.pagenum = vm.page;
+          console.log(vm.returned.pagenum);
+          console.log(vm.returned);
+          MovieService.getMovies(vm.returned).then(function() {
+            var newMovies = MovieService.setMovies();
+            vm.movies = vm.movies.concat(newMovies);
+            console.log(vm.movies);
+          });
+
+        }
       }
       vm.showMovies = function() {
         vm.movies = MovieService.setMovies();
@@ -20,6 +36,8 @@
         ListService.saveToList(movie);
         vm.nextMovie();
       }
+
+
 
       vm.nextMovieList = function () {
         console.log("next movie list called")
