0581444e8f3e408abc527fc56550b66d60abe850
Mohammed Fahad <mfahad24@users.noreply.github.com>
2018-03-08 11:40:04 -0500

    working on watch component

--- components/selectComponent.js
+++ components/selectComponent.js
@@ -1,7 +1,7 @@
 (function() {
   var selectComponent = {
     templateUrl: `partials/select.html`,
-    controller: function(MovieService, ListService) {
+    controller: function(MovieService, ListService, $location) {
       var vm = this;
       vm.returned = MovieService.returnParams();
       MovieService.getMovies(vm.returned);
@@ -39,7 +39,7 @@
 
 
 
-      vm.nextMovieList = function () {
+      vm.nextMovieList = function() {
         console.log("next movie list called")
         // vm.returned = MovieService.returnParams();
         // console.log(vm.returned);
@@ -53,7 +53,10 @@
       // }
 
       vm.showMovies();
-
+      vm.switchToWatch = function() {
+        $location.path("/watch");
+        console.log("switched");
+      }
     }
   }
 
