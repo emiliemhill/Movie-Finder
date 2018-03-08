bd1e94caf7d23dfb1d97b9607562ecab1f23fbec
Tom Bianchi <atbianchi@gmail.com>
2018-03-08 14:34:21 -0500

    Detail Service API call

--- DetailService.js
+++ DetailService.js
@@ -6,8 +6,17 @@
 
     }
 
-    function getMovieDetails(movieId) {
-
+    function getMovieDetails(movie) {
+      var movieID = movie.id
+      var baseUrl = "https://api.themoviedb.org/3/movie/"
+      var urlsuffix = "?api_key=a420712cee91c2aec196fe700c0ceb35&append_to_response=credits"
+      return $http({
+        method: "GET",
+        url: baseUrl + movieID + urlsuffix
+      }).then(function (response) {
+        // console.log("service", response.data.results);
+        return response.data.results;
+      });
     }
 
 
