8b3c39744ebd47d773f6ffa6f6693326f90c1994
Mohammed Fahad <mfahad24@users.noreply.github.com>
2018-03-12 15:24:59 -0400

    changes

--- Directives/MovieDirective.js
+++ Directives/MovieDirective.js
@@ -1,22 +1,20 @@
-(function() {
-  function fadeIn() {
-    return {
-      link: function($scope, $element, $attrs) {
-        $element.on("onLoad", function() {
-          console.log("Hello");
-          // $element.css("display", "none");
-          $element.fadeIn(500);
-
-      //   });
-      //   $element.on("mouseout", function (){
-      //     $element.css("color", "white")
-      //   })
-      // }
-    })
-  }
-}
-}
-  angular
-    .module("myApp")
-    .directive("fadeIn", fadeIn);
-})();
+// (function() {
+//   function fadeIn() {
+//     return {
+//       link: function($scope, $element, $attrs) {
+//         $element.on("click", function() {
+//           $element.fadeIn(500);
+//
+//       //   });
+//       //   $element.on("mouseout", function (){
+//       //     $element.css("color", "white")
+//       //   })
+//       // }
+//     })
+//   }
+// }
+// }
+//   angular
+//     .module("myApp")
+//     .directive("fadeIn", fadeIn);
+// })();
